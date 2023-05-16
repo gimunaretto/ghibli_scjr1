23d171cc057c9c80d2aeb01ad2d9884ea7c9bcf9
gimunaretto <61035475+gimunaretto@users.noreply.github.com>
2023-05-15 23:26:00 -0300

    Ajsute no nome

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Studio Ghibli',
       theme: ThemeData(
         fontFamily: 'QuickSand',
         colorScheme: ColorScheme.fromSwatch().copyWith(
